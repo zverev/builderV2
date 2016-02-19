1db0bf93a7fd2981b068060bdce3f835e4b5e9b0
zverev <alexander.zverev@gmail.com>
2016-02-19 15:46:43 +0300

    working dedupe plugin

--- dedupePlugin.js
+++ dedupePlugin.js
@@ -4,13 +4,15 @@ var path = require('path');
 var fs = require('fs');
 
 module.exports = function(br, opts) {
-    // 1. Go through all deps and determine the greatest libraries
+    // Go through all deps, determine the greatest libraries
     // versions and set pkgPaths hash.
     // ('package' event is emitted by browserify at deps phase)
 
     // Hash with package name as a key and package's package.json contents as a value.
     // If a package has less version, it will be replaced by the greater one.
     var topPkgs = {};
+    // { <package_name>: '<entry_file_path>' }
+    var topPkgsEntries = {};
     // Packages of libraries, that have less version
     var dropped = [];
     // {<package_dir>: <package_name>}
@@ -21,51 +23,47 @@ module.exports = function(br, opts) {
         var ePkg = topPkgs[pkg.name];
         if (!ePkg) {
             topPkgs[pkg.name] = pkg;
+            topPkgsEntries[pkg.name] = getEntryFilePath(pkg);
         } else if (semver.gt(pkg.version, ePkg.version)) {
             dropped.push(ePkg);
             topPkgs[pkg.name] = pkg;
+            topPkgsEntries[pkg.name] = getEntryFilePath(pkg);
         }
     });
 
-
-    // Rows buffer to push them to further streams
     var rows = [];
-    // Key = component id, value = components, that depend on it.
-    // {'./foo': ['./bar', './baz']}
-    var dependents = {};
     br.pipeline.get('deps').push(through2.obj(function(row, enc, next) {
+        // pushing row immediately will cause an error
+        // (?)
         rows.push(row);
         next();
-    }, function() {
-        var dependents = getDependents(rows, pkgsDirs);
-        console.log(dependents)
+    }, function(cb) {
+        rows.map(row => {
+            // replace library dependencies by the greatest ones
+            Object.keys(row.deps).map(function(dep) {
+                if (topPkgsEntries[dep]) {
+                    //debugger;
+                    if (topPkgsEntries[dep] !== row.deps[dep]) {
+                        row.deps[dep] = topPkgsEntries[dep];
+                    }
+                }
+            });
 
-        // flush rows
-        var stream = this;
-        rows.map(function(row) {
-            stream.push(row);
-        })
+            // Browserify will remove unused dependencies
+
+            // if a file is from dropped library, remove it's source
+            // dropped.map(d => {
+            //     if (row.file.indexOf(d.__dirname) > -1) {
+            //         row.source = '/* DROPPED */'
+            //     }
+            // });
+
+            this.push(row);
+        });
+        cb();
     }));
 };
 
-function getDependents(rows, pkgsDirs) {
-    var gli = getLibId;
-    var dependents = {};
-    rows.map(function(row) {
-        Object.keys(row.deps).map(function(reqBody) {
-            var id = row.deps[reqBody];
-            // var id = getLib Id(path, pkgsDirs);
-            if (!dependents[id]) {
-                dependents[id] = [];
-            }
-            dependents[id].push(row.id);
-        });
-    });
-    return dependents;
-}
-
-function getLibId(pth, pkgsDirs) {
-    return pkgsDirs[Object.keys(pkgsDirs).find(function(pkgDir) {
-        return path.dirname(pth) === pkgDir;
-    })];
+function getEntryFilePath(pkg) {
+    return path.join(pkg.__dirname, pkg.main || 'index.js');
 }
